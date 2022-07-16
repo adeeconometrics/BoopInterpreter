365d304bab03639ddd3b33d768c0641ae6d3da8e
Dave Amiana <amianism25@gmail.com>
2022-07-17 00:22:17 +0800

    ast definition and declaration

--- include/ASTNodes.h
+++ include/ASTNodes.h
@@ -3,7 +3,6 @@
 
 #include "Token.h"
 #include "Types.h"
-#include "Uncopyable.h"
 
 #include <memory>
 #include <optional>
@@ -16,66 +15,77 @@
  * VTables and gets rid of performance overhead vtables incurr
  *
  */
-namespace boop::AST {
+namespace cpplox::AST {
 
 // forward declaration of expression node types
 struct ExprBinary;
-struct ExprAssign;
-struct ExprCall;
-struct ExprGet;
 struct ExprGrouping;
 struct ExprLiteral;
-struct ExprLogical;
-struct ExprSet;
-struct ExprSuper;
-struct ExprThis;
 struct ExprUnary;
-struct ExprVariable;
+struct ExprConditional;
 struct ExprPostfix;
+struct ExprVariable;
+struct ExprAssignment;
+struct ExprLogical;
+struct ExprCall;
+struct ExprFunction;
+struct ExprGet;
+struct ExprSet;
+struct ExprThis;
+struct ExprSuper;
 
-// reference manager assign to smart pointer
+// reference manager assigned to smart pointer 
 using ExprBinaryPtr = std::unique_ptr<ExprBinary>;
-using ExprAssignPtr = std::unique_ptr<ExprAssign>;
-using ExprCallPtr = std::unique_ptr<ExprCall>;
-using ExprGetPtr = std::unique_ptr<ExprGet>;
 using ExprGroupingPtr = std::unique_ptr<ExprGrouping>;
 using ExprLiteralPtr = std::unique_ptr<ExprLiteral>;
-using ExprLogicalPtr = std::unique_ptr<ExprLogical>;
-using ExprSetPtr = std::unique_ptr<ExprSet>;
-using ExprSuperPtr = std::unique_ptr<ExprSuper>;
-using ExprThisPtr = std::unique_ptr<ExprThis>;
 using ExprUnaryPtr = std::unique_ptr<ExprUnary>;
-using ExprVariablePtr = std::unique_ptr<ExprVariable>;
+using ExprConditionalPtr = std::unique_ptr<ExprConditional>;
 using ExprPostfixPtr = std::unique_ptr<ExprPostfix>;
-
+using ExprVariablePtr = std::unique_ptr<ExprVariable>;
+using ExprAssignmentPtr = std::unique_ptr<ExprAssignment>;
+using ExprLogicalPtr = std::unique_ptr<ExprLogical>;
+using ExprCallPtr = std::unique_ptr<ExprCall>;
+using ExprFunctionPtr = std::unique_ptr<ExprFunction>;
+using ExprGetPtr = std::unique_ptr<ExprGet>;
+using ExprSetPtr = std::unique_ptr<ExprSet>;
+using ExprThisPtr = std::unique_ptr<ExprThis>;
+using ExprSuperPtr = std::unique_ptr<ExprSuper>;
 
 // union type for reference manager to handle statement nodes
 using ExprPtrVariant =
-    std::variant<ExprAssign, ExprBinary, ExprCall, ExprGet, ExprGrouping,
-                 ExprLiteral, ExprLogical, ExprSet, ExprSuper, ExprThis,
-                 ExprUnary, ExprVariable, ExprPostfix>;
+    std::variant<ExprBinaryPtr, ExprGroupingPtr, ExprLiteralPtr, ExprUnaryPtr,
+                 ExprConditionalPtr, ExprPostfixPtr, ExprVariablePtr,
+                 ExprAssignmentPtr, ExprLogicalPtr, ExprCallPtr, ExprFunctionPtr,
+                 ExprGetPtr, ExprSetPtr, ExprThisPtr, ExprSuperPtr>;
 
-    // methods to visit expression types in AST
-
-    auto make_binary_expr(ExprPtrVariant lhs, Token op, ExprPtrVariant rhs)
-        -> ExprPtrVariant;
-auto make_unary_expr(Token op, ExprPtrVariant rhs) -> ExprPtrVariant;
-auto make_call_expr(ExprPtrVariant callee, Token parent,
-                    std::vector<ExprPtrVariant> args) -> ExprPtrVariant;
+// methods that creates expression nodes 
+auto make_binary_expr(ExprPtrVariant left, Token op, ExprPtrVariant right)
+    -> ExprPtrVariant;
+auto make_unary_expr(Token op, ExprPtrVariant right) -> ExprPtrVariant;
+auto make_grouping_expr(ExprPtrVariant right) -> ExprPtrVariant;
+auto make_literal_expr(OptionalLiteral literal) -> ExprPtrVariant;
+auto make_conditional_expr(ExprPtrVariant condition, ExprPtrVariant then,
+                          ExprPtrVariant elseBranch) -> ExprPtrVariant;
+auto make_postfix_expr(ExprPtrVariant left, Token op) -> ExprPtrVariant;
+auto make_variable_expr(Token varName) -> ExprPtrVariant;
+auto make_assignment_expr(Token varName, ExprPtrVariant expr) -> ExprPtrVariant;
+auto make_logical_expr(ExprPtrVariant left, Token op, ExprPtrVariant right)
+    -> ExprPtrVariant;
+auto make_call_expr(ExprPtrVariant callee, Token paren,
+                   std::vector<ExprPtrVariant> arguments) -> ExprPtrVariant;
+auto make_function_expr(std::vector<Token> params,
+                   std::vector<StmtPtrVariant> fnBody) -> ExprPtrVariant;
 auto make_get_expr(ExprPtrVariant expr, Token name) -> ExprPtrVariant;
-auto make_group_expr() -> ExprPtrVariant;
-auto make_literal_expr() -> ExprPtrVariant;
-auto make_set_expr() -> ExprPtrVariant;
-auto make_super_expr() -> ExprPtrVariant;
-auto make_this_expr() -> ExprPtrVariant;
-auto make_assign_expr(Token var_name, ExprPtrVariant expr) -> ExprPtrVariant;
-auto make_variable_expr() -> ExprPtrVariant;
+auto make_set_expr(ExprPtrVariant expr, Token name, ExprPtrVariant value)
+    -> ExprPtrVariant;
+auto make_this_expr(Token keyword) -> ExprPtrVariant;
+auto make_super_expr(Token keyword, Token method) -> ExprPtrVariant;
 
 // forward declaration of statement node types
 struct StmtExpr;
 struct StmtPrint;
 struct StmtBlock;
-struct StmtVar;
+struct StmtVariable;
 struct StmtIf;
 struct StmtWhile;
 struct StmtFor;
@@ -83,23 +93,207 @@ struct StmtFunction;
 struct StmtReturn;
 struct StmtClass;
 
-// reference manager assigned to handle node types
-using StmtExprPtr = std::unique_ptr<StmtExpr>;
-using StmtPrintPtr = std::unique_ptr<StmtPrint>;
-using StmtBlockPtr = std::unique_ptr<StmtBlock>;
-using StmtVarPtr = std::unique_ptr<StmtVar>;
-using StmtIfPtr = std::unique_ptr<StmtIf>;
-using StmtWhilePtr = std::unique_ptr<StmtWhile>;
-using StmtForPtr = std::unique_ptr<StmtFor>;
-using StmtFunctionPtr = std::unique_ptr<StmtFunction>;
-using StmtReturnPtr = std::unique_ptr<StmtReturn>;
-using StmtClassPtr = std::unique_ptr<StmtClass>;
+// alias for automatic reference managers on each statement node types
+using ExprStmtPtr = std::unique_ptr<StmtExpr>;
+using PrintStmtPtr = std::unique_ptr<StmtPrint>;
+using BlockStmtPtr = std::unique_ptr<StmtBlock>;
+using VarStmtPtr = std::unique_ptr<StmtVariable>;
+using IfStmtPtr = std::unique_ptr<StmtIf>;
+using WhileStmtPtr = std::unique_ptr<StmtWhile>;
+using ForStmtPtr = std::unique_ptr<StmtFor>;
+using FuncStmtPtr = std::unique_ptr<StmtFunction>;
+using RetStmtPtr = std::unique_ptr<StmtReturn>;
+using ClassStmtPtr = std::unique_ptr<StmtClass>;
 
 // union type for reference manager to handle statement nodes
 using StmtPtrVariant =
-    std::variant<StmtExpr, StmtPrint, StmtBlock, StmtVar, StmtIf, StmtWhile,
-                 StmtFor, StmtFunction, StmtReturn, StmtClass>
+    std::variant<ExprStmtPtr, PrintStmtPtr, BlockStmtPtr, VarStmtPtr, IfStmtPtr,
+                 WhileStmtPtr, ForStmtPtr, FuncStmtPtr, RetStmtPtr,
+                 ClassStmtPtr>;
 
-} // namespace boop::AST
+
+// methods that creates statement nodes
+auto make_expr_stmt(ExprPtrVariant expr) -> StmtPtrVariant;
+auto make_print_stmt(ExprPtrVariant expr) -> StmtPtrVariant;
+auto make_block_stmt(std::vector<StmtPtrVariant> statements) -> StmtPtrVariant;
+auto make_var_stmt(Token varName, std::optional<ExprPtrVariant> initializer)
+    -> StmtPtrVariant;
+auto make_if_stmt(ExprPtrVariant condition, StmtPtrVariant thenBranch,
+                 std::optional<StmtPtrVariant> elseBranch) -> StmtPtrVariant;
+auto make_while_stmt(ExprPtrVariant condition, StmtPtrVariant loopBody)
+    -> StmtPtrVariant;
+auto make_for_stmt(std::optional<StmtPtrVariant> initializer,
+                  std::optional<ExprPtrVariant> condition,
+                  std::optional<ExprPtrVariant> increment,
+                  StmtPtrVariant loopBody) -> StmtPtrVariant;
+auto make_function_stmt(Token fName, ExprFunctionPtr ExprFunction) -> StmtPtrVariant;
+auto make_return_stmt(Token ret, std::optional<ExprPtrVariant> value)
+    -> StmtPtrVariant;
+auto make_class_stmt(Token className, std::optional<ExprPtrVariant> superClass,
+                    std::vector<StmtPtrVariant> methods) -> StmtPtrVariant;
+
+
+// Expression AST Types member declaration:
+struct ExprBinary final : public Uncopyable {
+  ExprPtrVariant left;
+  Token op;
+  ExprPtrVariant right;
+  ExprBinary(ExprPtrVariant left, Token op, ExprPtrVariant right);
+};
+
+struct ExprGrouping final : public Uncopyable {
+  ExprPtrVariant expression;
+  explicit ExprGrouping(ExprPtrVariant expression);
+};
+
+struct ExprLiteral final : public Uncopyable {
+  OptionalLiteral literalVal;
+  explicit ExprLiteral(OptionalLiteral value);
+};
+
+struct ExprUnary final : public Uncopyable {
+  Token op;
+  ExprPtrVariant right;
+  ExprUnary(Token op, ExprPtrVariant right);
+};
+
+struct ExprConditional final : public Uncopyable {
+  ExprPtrVariant condition;
+  ExprPtrVariant thenBranch;
+  ExprPtrVariant elseBranch;
+  ExprConditional(ExprPtrVariant condition, ExprPtrVariant thenBranch,
+                  ExprPtrVariant elseBranch);
+};
+
+struct ExprPostfix final : public Uncopyable {
+  ExprPtrVariant left;
+  Token op;
+  ExprPostfix(ExprPtrVariant left, Token op);
+};
+
+struct ExprVariable final : public Uncopyable {
+  Token varName;
+  explicit ExprVariable(Token varName);
+};
+
+struct ExprAssignment final : public Uncopyable {
+  Token varName;
+  ExprPtrVariant right;
+  ExprAssignment(Token varName, ExprPtrVariant right);
+};
+
+struct ExprLogical final : public Uncopyable {
+  ExprPtrVariant left;
+  Token op;
+  ExprPtrVariant right;
+  ExprLogical(ExprPtrVariant left, Token op, ExprPtrVariant right);
+};
+
+struct ExprCall final : public Uncopyable {
+  ExprPtrVariant callee;
+  Token paren;
+  std::vector<ExprPtrVariant> arguments;
+  ExprCall(ExprPtrVariant callee, Token paren,
+           std::vector<ExprPtrVariant> arguments);
+};
+
+struct ExprFunction final : public Uncopyable {
+  std::vector<Token> parameters;
+  std::vector<StmtPtrVariant> body;
+  ExprFunction(std::vector<Token> parameters, std::vector<StmtPtrVariant> body);
+};
+
+struct ExprGet final : public Uncopyable {
+  ExprPtrVariant expr;
+  Token name;
+  ExprGet(ExprPtrVariant expr, Token name);
+};
+
+struct ExprSet final : public Uncopyable {
+  ExprPtrVariant expr;
+  Token name;
+  ExprPtrVariant value;
+  ExprSet(ExprPtrVariant expr, Token name, ExprPtrVariant value);
+};
+
+struct ExprThis final : public Uncopyable {
+  Token keyword;
+  explicit ExprThis(Token keyword);
+};
+
+struct ExprSuper final : public Uncopyable {
+  Token keyword;
+  Token method;
+  explicit ExprSuper(Token keyword, Token method);
+};
+
+// Statment AST types declaration
+struct StmtExpr final : public Uncopyable {
+  ExprPtrVariant expression;
+  explicit StmtExpr(ExprPtrVariant expr);
+};
+
+struct StmtPrint final : public Uncopyable {
+  ExprPtrVariant expression;
+  explicit StmtPrint(ExprPtrVariant expression);
+};
+
+struct StmtBlock final : public Uncopyable {
+  std::vector<StmtPtrVariant> statements;
+  explicit StmtBlock(std::vector<StmtPtrVariant> statements);
+};
+
+struct StmtVariable final : public Uncopyable {
+  Token varName;
+  std::optional<ExprPtrVariant> initializer;
+  explicit StmtVariable(Token varName, std::optional<ExprPtrVariant> initializer);
+};
+
+struct StmtIf final : public Uncopyable {
+  ExprPtrVariant condition;
+  StmtPtrVariant thenBranch;
+  std::optional<StmtPtrVariant> elseBranch;
+  explicit StmtIf(ExprPtrVariant condition, StmtPtrVariant thenBranch,
+                  std::optional<StmtPtrVariant> elseBranch);
+};
+
+struct StmtWhile final : public Uncopyable {
+  ExprPtrVariant condition;
+  StmtPtrVariant loopBody;
+  explicit StmtWhile(ExprPtrVariant condition, StmtPtrVariant loopBody);
+};
+
+struct StmtFor final : public Uncopyable {
+  std::optional<StmtPtrVariant> initializer;
+  std::optional<ExprPtrVariant> condition;
+  std::optional<ExprPtrVariant> increment;
+  StmtPtrVariant loopBody;
+  explicit StmtFor(std::optional<StmtPtrVariant> initializer,
+                   std::optional<ExprPtrVariant> condition,
+                   std::optional<ExprPtrVariant> increment,
+                   StmtPtrVariant loopBody);
+};
+
+struct StmtFunction : public Uncopyable {
+  Token funcName;
+  ExprFunctionPtr ExprFunction;
+  StmtFunction(Token funcName, ExprFunctionPtr ExprFunction);
+};
+
+struct StmtReturn : public Uncopyable {
+  Token ret;
+  std::optional<ExprPtrVariant> value;
+  StmtReturn(Token ret, std::optional<ExprPtrVariant> value);
+};
+
+struct StmtClass : public Uncopyable {
+  Token className;
+  std::optional<ExprPtrVariant> superClass;
+  std::vector<StmtPtrVariant> methods;
+  StmtClass(Token className, std::optional<ExprPtrVariant> superClass,
+            std::vector<StmtPtrVariant> methods);
+};
+
+} // namespace cpplox::AST
 
 #endif // __ASTNODES_H__
---
+++ src/ASTNodes.cpp
@@ -0,0 +1,218 @@
+#include "ASTNodes.h"
+
+
+namespace boop::AST {
+ExprBinary::ExprBinary(ExprPtrVariant left, Token op, ExprPtrVariant right)
+    : left(std::move(left)), op(std::move(op)), right(std::move(right)) {}
+
+ExprGrouping::ExprGrouping(ExprPtrVariant expression)
+    : expression(std::move(expression)) {}
+
+ExprLiteral::ExprLiteral(OptionalLiteral value)
+    : literalVal(std::move(value)) {}
+
+ExprUnary::ExprUnary(Token op, ExprPtrVariant right)
+    : op(std::move(op)), right(std::move(right)) {}
+
+ExprConditional::ExprConditional(ExprPtrVariant condition,
+                                 ExprPtrVariant thenBranch,
+                                 ExprPtrVariant elseBranch)
+    : condition(std::move(condition)), thenBranch(std::move(thenBranch)),
+      elseBranch(std::move(elseBranch)) {}
+
+ExprPostfix::ExprPostfix(ExprPtrVariant left, Token op)
+    : left(std::move(left)), op(std::move(op)) {}
+
+ExprVariable::ExprVariable(Token varName) : varName(std::move(varName)) {}
+
+ExprAssignment::ExprAssignment(Token varName, ExprPtrVariant right)
+    : varName(std::move(varName)), right(std::move(right)) {}
+
+ExprLogical::ExprLogical(ExprPtrVariant left, Token op, ExprPtrVariant right)
+    : left(std::move(left)), op(std::move(op)), right(std::move(right)) {}
+
+ExprCall::ExprCall(ExprPtrVariant callee, Token paren,
+                   std::vector<ExprPtrVariant> arguments)
+    : callee(std::move(callee)), paren(std::move(paren)),
+      arguments(std::move(arguments)) {}
+
+ExprFunction::ExprFunction(std::vector<Token> parameters,
+                   std::vector<StmtPtrVariant> body)
+    : parameters(std::move(parameters)), body(std::move(body)) {}
+
+ExprGet::ExprGet(ExprPtrVariant expr, Token name)
+    : expr(std::move(expr)), name(std::move(name)) {}
+
+ExprSet::ExprSet(ExprPtrVariant expr, Token name, ExprPtrVariant value)
+    : expr(std::move(expr)), name(std::move(name)), value(std::move(value)) {}
+
+ExprThis::ExprThis(Token keyword) : keyword(std::move(keyword)) {}
+
+ExprSuper::ExprSuper(Token keyword, Token method)
+    : keyword(std::move(keyword)), method(std::move(method)) {}
+
+// ==============================//
+// EPV creation helper functions //
+// ==============================//
+auto make_binary_expr(ExprPtrVariant left, Token op, ExprPtrVariant right)
+    -> ExprPtrVariant {
+  return std::make_unique<ExprBinary>(std::move(left), op, std::move(right));
+}
+
+auto make_unary_expr(Token op, ExprPtrVariant right) -> ExprPtrVariant {
+  return std::make_unique<ExprUnary>(op, std::move(right));
+}
+
+auto make_grouping_expr(ExprPtrVariant right) -> ExprPtrVariant {
+  return std::make_unique<ExprGrouping>(std::move(right));
+}
+
+auto make_literal_expr(OptionalLiteral literal) -> ExprPtrVariant {
+  return std::make_unique<ExprLiteral>(std::move(literal));
+}
+
+auto make_conditional_expr(ExprPtrVariant condition, ExprPtrVariant then,
+                          ExprPtrVariant elseBranch) -> ExprPtrVariant {
+  return std::make_unique<ExprConditional>(
+      std::move(condition), std::move(then), std::move(elseBranch));
+}
+
+auto make_postfix_expr(ExprPtrVariant left, Token op) -> ExprPtrVariant {
+  return std::make_unique<ExprPostfix>(std::move(left), op);
+}
+
+auto make_variable_expr(Token varName) -> ExprPtrVariant {
+  return std::make_unique<ExprVariable>(varName);
+}
+
+auto make_assignment_expr(Token varName, ExprPtrVariant expr) -> ExprPtrVariant {
+  return std::make_unique<ExprAssignment>(varName, std::move(expr));
+}
+
+auto make_logical_expr(ExprPtrVariant left, Token op, ExprPtrVariant right)
+    -> ExprPtrVariant {
+  return std::make_unique<ExprLogical>(std::move(left), op, std::move(right));
+}
+
+auto make_call_expr(ExprPtrVariant callee, Token paren,
+                   std::vector<ExprPtrVariant> arguments) -> ExprPtrVariant {
+  return std::make_unique<ExprCall>(std::move(callee), std::move(paren),
+                                    std::move(arguments));
+}
+
+auto make_function_expr(std::vector<Token> params,
+                   std::vector<StmtPtrVariant> fnBody) -> ExprPtrVariant {
+  return std::make_unique<ExprFunction>(std::move(params), std::move(fnBody));
+}
+
+auto make_get_expr(ExprPtrVariant expr, Token name) -> ExprPtrVariant {
+  return std::make_unique<ExprGet>(std::move(expr), std::move(name));
+}
+
+auto make_set_expr(ExprPtrVariant expr, Token name, ExprPtrVariant value)
+    -> ExprPtrVariant {
+  return std::make_unique<ExprSet>(std::move(expr), std::move(name),
+                                   std::move(value));
+}
+
+auto make_this_expr(Token keyword) -> ExprPtrVariant {
+  return std::make_unique<ExprThis>(std::move(keyword));
+}
+
+auto make_super_expr(Token keyword, Token method) -> ExprPtrVariant {
+  return std::make_unique<ExprSuper>(std::move(keyword), std::move(method));
+}
+
+// =================== //
+// Statment AST types; //
+// =================== //
+StmtExpr::StmtExpr(ExprPtrVariant expr) : expression(std::move(expr)) {}
+
+StmtPrint::StmtPrint(ExprPtrVariant expr) : expression(std::move(expr)) {}
+
+StmtBlock::StmtBlock(std::vector<StmtPtrVariant> statements)
+    : statements(std::move(statements)) {}
+
+VarStmt::VarStmt(Token varName, std::optional<ExprPtrVariant> initializer)
+    : varName(std::move(varName)), initializer(std::move(initializer)) {}
+
+StmtIf::StmtIf(ExprPtrVariant condition, StmtPtrVariant thenBranch,
+               std::optional<StmtPtrVariant> elseBranch)
+    : condition(std::move(condition)), thenBranch(std::move(thenBranch)),
+      elseBranch(std::move(elseBranch)) {}
+
+StmtWhile::StmtWhile(ExprPtrVariant condition, StmtPtrVariant loopBody)
+    : condition(std::move(condition)), loopBody(std::move(loopBody)) {}
+
+StmtFor::StmtFor(std::optional<StmtPtrVariant> initializer,
+                 std::optional<ExprPtrVariant> condition,
+                 std::optional<ExprPtrVariant> increment,
+                 StmtPtrVariant loopBody)
+    : initializer(std::move(initializer)), condition(std::move(condition)),
+      increment(std::move(increment)), loopBody(std::move(loopBody)) {}
+
+StmtFunction::StmtFunction(Token funcName, ExprFunctionPtr ExprFunction)
+    : funcName(std::move(funcName)), ExprFunction(std::move(ExprFunction)) {}
+
+StmtReturn::StmtReturn(Token ret, std::optional<ExprPtrVariant> value)
+    : ret(std::move(ret)), value(std::move(value)) {}
+
+StmtClass::StmtClass(Token className, std::optional<ExprPtrVariant> superClass,
+                     std::vector<StmtPtrVariant> methods)
+    : className(std::move(className)), superClass(std::move(superClass)),
+      methods(std::move(methods)) {}
+
+// ============================================================= //
+// Helper functions to create StmtPtrVariants for each Stmt type //
+// ============================================================= //
+auto make_expr_stmt(ExprPtrVariant expr) -> StmtPtrVariant {
+  return std::make_unique<StmtExpr>(std::move(expr));
+}
+
+auto make_print_stmt(ExprPtrVariant expr) -> StmtPtrVariant {
+  return std::make_unique<StmtPrint>(std::move(expr));
+}
+
+auto make_block_stmt(std::vector<StmtPtrVariant> statements) -> StmtPtrVariant {
+  return std::make_unique<StmtBlock>(std::move(statements));
+}
+
+auto make_var_stmt(Token varName, std::optional<ExprPtrVariant> initializer)
+    -> StmtPtrVariant {
+  return std::make_unique<VarStmt>(varName, std::move(initializer));
+}
+
+auto make_if_stmt(ExprPtrVariant condition, StmtPtrVariant thenBranch,
+                 std::optional<StmtPtrVariant> elseBranch) -> StmtPtrVariant {
+  return std::make_unique<StmtIf>(std::move(condition), std::move(thenBranch),
+                                  std::move(elseBranch));
+}
+
+auto make_while_stmt(ExprPtrVariant condition, StmtPtrVariant loopBody)
+    -> StmtPtrVariant {
+  return std::make_unique<StmtWhile>(std::move(condition), std::move(loopBody));
+}
+
+auto make_for_stmt(std::optional<StmtPtrVariant> initializer,
+                  std::optional<ExprPtrVariant> condition,
+                  std::optional<ExprPtrVariant> increment,
+                  StmtPtrVariant loopBody) -> StmtPtrVariant {
+  return std::make_unique<StmtFor>(std::move(initializer), std::move(condition),
+                                   std::move(increment), std::move(loopBody));
+}
+
+auto make_function_stmt(Token fName, ExprFunctionPtr ExprFunction) -> StmtPtrVariant {
+  return std::make_unique<StmtFunction>(std::move(fName), std::move(ExprFunction));
+}
+
+auto make_return_stmt(Token ret, std::optional<ExprPtrVariant> value)
+    -> StmtPtrVariant {
+  return std::make_unique<StmtReturn>(std::move(ret), std::move(value));
+}
+
+auto make_class_stmt(Token className, std::optional<ExprPtrVariant> superClass,
+                    std::vector<StmtPtrVariant> methods) -> StmtPtrVariant {
+  return std::make_unique<StmtClass>(std::move(className),
+                                     std::move(superClass), std::move(methods));
+}
+}
